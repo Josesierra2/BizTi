86e114c149fa55503df202984209bcaf0e939843
Leandro <rodriguezleandro998@gmail.com>
2026-08-28 08:09:50 -0500

    se crea proyecto

---
+++ src/main/java/com/example/BizTi/BizTiApplication.java
@@ -0,0 +1,13 @@
+package com.example.BizTi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizTiApplication.class, args);
+	}
+
+}
